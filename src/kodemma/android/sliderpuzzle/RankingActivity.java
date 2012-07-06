fe3b922525b18cb3a40d0229b9d52c9e871ae155
nakayama hiroshi <hrs.nkym@gmail.com>
2012-07-06 14:31:35 +0900

    local repo commit.

--- src/kodemma/android/sliderpuzzle/RankingActivity.java
+++ src/kodemma/android/sliderpuzzle/RankingActivity.java
@@ -1,5 +1,21 @@
 package kodemma.android.sliderpuzzle;
 
-public class RankingActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+public class RankingActivity extends Activity {
+	Button bt;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		LinearLayout ll = new LinearLayout(this);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		setContentView(ll);
+
+		bt = new Button(this);
+		bt.setText("Ranking");
+		
+		ll.addView(bt);
+	}
 }
--- src/kodemma/android/sliderpuzzle/TitleActivity.java
+++ src/kodemma/android/sliderpuzzle/TitleActivity.java
@@ -1,5 +1,21 @@
 package kodemma.android.sliderpuzzle;
 
-public class TitleActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+public class TitleActivity extends Activity {
+	Button bt;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		LinearLayout ll = new LinearLayout(this);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		setContentView(ll);
+
+		bt = new Button(this);
+		bt.setText("Title Activity");
+		
+		ll.addView(bt);
+	}
 }
