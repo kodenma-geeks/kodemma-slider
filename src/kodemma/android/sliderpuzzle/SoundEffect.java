b697102cf6270315000854baf535de87b0c1c9c2
yuji_shimada <yuji.shimada@gmail.com>
2012-07-06 10:32:04 +0900

    追加されていなかったのでSoundEffectクラスとrawフォルダを追加

---
+++ src/kodemma/android/sliderpuzzle/SoundEffect.java
@@ -0,0 +1,35 @@
+package kodemma.android.sliderpuzzle;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+public class SoundEffect {
+	static SoundPool mSoundPool;
+//	static int sound_Touch;
+//	static int sound_Move;
+	static int sound_Up;
+	static int sound_Button_on;
+	static int sound_Button_off;
+	
+	public static void soundLoad(Context cn){
+		mSoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+//		sound_Touch = mSoundPool.load(cn, R.raw.switch_ti, 1);
+//		sound_Move = mSoundPool.load(this, R.raw., 1);
+		sound_Up = mSoundPool.load(cn, R.raw.switch_ti, 1);
+		sound_Button_on = mSoundPool.load(cn, R.raw.turi, 1);
+		sound_Button_off = mSoundPool.load(cn, R.raw.pipon, 1);
+	}
+	public static void soundStop(){
+//		mSoundPool.unload(sound_Touch);
+//		mSoundPool.unload(sound_Move);
+		mSoundPool.unload(sound_Up);
+		mSoundPool.unload(sound_Button_on);
+		mSoundPool.unload(sound_Button_off);
+		mSoundPool.release();
+	}
+	public static void getSound(int sound_source){
+		mSoundPool.play(sound_source, 0.3f, 0.3f, 1, 0, 1.0f);
+	}
+}
+
