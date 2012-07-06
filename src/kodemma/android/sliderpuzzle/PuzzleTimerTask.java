12f82e22c312f60d8b3ab373198a3733bf16c47e
yuji_shimada <yuji.shimada@gmail.com>
2012-07-06 14:27:28 +0900

    PuzzleTimerTaskのロジックを変更

--- src/kodemma/android/sliderpuzzle/PuzzleTimerTask.java
+++ src/kodemma/android/sliderpuzzle/PuzzleTimerTask.java
@@ -1,48 +1,50 @@
 package kodemma.android.sliderpuzzle;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import android.os.Handler;
 import android.widget.TextView;
 
-public class PuzzleTimerTask extends TimerTask{
+public class PuzzleTimerTask extends TimerTask {
 
 	private PuzzleTimerTask timerTask;
 	private Timer puzzleTimer;
 	static long lapTime;
 	TextView displayed;
-	
+
 	final Handler hn = new Handler();
-	
-	public PuzzleTimerTask(TextView disp){
+
+	public PuzzleTimerTask(TextView disp) {
 		super();
 		displayed = disp;
 	}
-	public void run(){
-   	hn.post(new Runnable(){
-	    	public void run(){
 
-	    		lapTime += 100; 		
-	    		Date date = new Date(lapTime-32400000); 
-	    		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-	    		displayed.setText(format.format(date)); 
+	public void run() {
+		hn.post(new Runnable() {
+			public void run() {
 
-	    	}
-    	});
+				lapTime += 100;
+				
+				long time = lapTime;
+				time /= 1000;
+		        long second = time % 60;
+		        time /= 60;
+		        long minute = time % 60;
+		        long hour = time / 60;
+	
+				displayed.setText(String.format("%02d:%02d:%02d", hour, minute, second));
+				
+			}
+		});
 	}
-	public void timerStart(){
-		if(puzzleTimer == null){
-			SoundEffect.getSound(SoundEffect.sound_Button_on);
+
+	public void timerStart() {
+		if (puzzleTimer == null) {
 			lapTime = 0;
 			timerTask = new PuzzleTimerTask(displayed);
 			puzzleTimer = new Timer(true);
 			puzzleTimer.scheduleAtFixedRate(timerTask, 100, 100);
-		}
-		else if(puzzleTimer != null){
-			SoundEffect.getSound(SoundEffect.sound_Button_off);
+		} else if (puzzleTimer != null) {
 			puzzleTimer.cancel();
 			lapTime = 0;
 			timerTask = new PuzzleTimerTask(displayed);
@@ -50,25 +52,27 @@ public class PuzzleTimerTask extends TimerTask{
 			puzzleTimer.scheduleAtFixedRate(timerTask, 100, 100);
 		}
 	}
-	public void timerPause(){
-		if(puzzleTimer != null){
+
+	public void timerPause() {
+		if (puzzleTimer != null) {
 			puzzleTimer.cancel();
 			puzzleTimer = null;
 		}
 	}
-	public void timerResume(){
-		if(puzzleTimer == null){
+
+	public void timerResume() {
+		if (puzzleTimer == null) {
 			timerTask = new PuzzleTimerTask(displayed);
 			puzzleTimer = new Timer(true);
 			puzzleTimer.scheduleAtFixedRate(timerTask, 100, 100);
 		}
 	}
-	public void timerStop(){
-		if(puzzleTimer != null){
+
+	public void timerStop() {
+		if (puzzleTimer != null) {
 			puzzleTimer.cancel();
 			puzzleTimer = null;
 			lapTime = 0;
 		}
 	}
 }
-
--- src/kodemma/android/sliderpuzzle/SoundEffect.java
+++ src/kodemma/android/sliderpuzzle/SoundEffect.java
@@ -11,14 +11,16 @@ public class SoundEffect {
 	static int sound_Up;
 	static int sound_Button_on;
 	static int sound_Button_off;
+	static int sound_solved;
 	
 	public static void soundLoad(Context cn){
 		mSoundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
 //		sound_Touch = mSoundPool.load(cn, R.raw.switch_ti, 1);
 //		sound_Move = mSoundPool.load(this, R.raw., 1);
 		sound_Up = mSoundPool.load(cn, R.raw.switch_ti, 1);
-		sound_Button_on = mSoundPool.load(cn, R.raw.turi, 1);
-		sound_Button_off = mSoundPool.load(cn, R.raw.pipon, 1);
+		sound_Button_on = mSoundPool.load(cn, R.raw.pipon, 1);
+		sound_Button_off = mSoundPool.load(cn, R.raw.popin, 1);
+		sound_solved = mSoundPool.load(cn, R.raw.turururu, 1);
 	}
 	public static void soundStop(){
 //		mSoundPool.unload(sound_Touch);
@@ -26,6 +28,7 @@ public class SoundEffect {
 		mSoundPool.unload(sound_Up);
 		mSoundPool.unload(sound_Button_on);
 		mSoundPool.unload(sound_Button_off);
+		mSoundPool.unload(sound_solved);
 		mSoundPool.release();
 	}
 	public static void getSound(int sound_source){
