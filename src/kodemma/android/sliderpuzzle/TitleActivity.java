6627aade5b5edbb78a66e2aec08af27191d165c7
nakayama hiroshi <hrs.nkym@gmail.com>
2012-07-11 16:17:15 +0900

    TitleActivity追加

---
+++ src/kodemma/android/sliderpuzzle/TitleActivity.java
@@ -0,0 +1,58 @@
+package kodemma.android.sliderpuzzle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class TitleActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.title);
+		setMoveActivity();
+	}
+
+	private void setMoveActivity() {
+		ImageView image1 = (ImageView) findViewById(R.id.imageView2);
+		ImageView image2 = (ImageView) findViewById(R.id.imageView3);
+		ImageView image3 = (ImageView) findViewById(R.id.imageView4);
+		ImageView image4 = (ImageView) findViewById(R.id.imageView5);
+
+		image1.setClickable(true);
+		image2.setClickable(true);
+		image3.setClickable(true);
+		image4.setClickable(true);
+
+		image1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				 Intent intent = new Intent(TitleActivity.this,
+				 BoardActivity.class);
+				 startActivity(intent);
+			}
+		});
+		image2.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+//				 Intent intent = new Intent(TitleActivity.this,
+//				 RankingActivity.class);
+//				 startActivity(intent);
+
+			}
+		});
+		image3.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				 Intent intent = new Intent(TitleActivity.this,
+				 SelectLevelActivity.class);
+				 startActivity(intent);
+
+			}
+		});
+		image4.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+	}
+}
