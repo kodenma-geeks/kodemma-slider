17b4c2d502619ffe0cd5733e3fa54b6041519f9b
Owner <Owner@BE59>
2012-07-13 18:37:11 +0900

    7/13 浜田　得点の修正

--- src/kodemma/android/sliderpuzzle/Level.java
+++ src/kodemma/android/sliderpuzzle/Level.java
@@ -9,7 +9,7 @@ public class Level {
 	public static final int MIN = 3;
 	public static final int MAX = 6;
 	private static final SortedMap<Integer, Level> levels = createLevels(MIN, MAX);
-	private final int BONUS = 10000000;		// スコアが適度な値になるためのボーナス値
+	private final int BONUS = 10000;		// スコアが適度な値になるためのボーナス値
 	private final int PANEL_VALUE = 120;	// パネル１枚ごとに与えられる点数
 
 	private int small;
@@ -27,8 +27,8 @@ public class Level {
 	public int score(long ms, int count) {
 		int score = 0;
 		try{
-//			int time = (int)ms/1000;	// ミリ秒から秒へ変換　7/13　やめ
-			score = (int)((BONUS / count) * (tiles() * PANEL_VALUE) / ms);// ミリ秒のまま計算
+			int time = (int)ms/1000;	// ミリ秒から秒へ変換　7/13　やめ
+			score = (int)((BONUS / count) * (tiles() * PANEL_VALUE) / time);
 		} catch (ArithmeticException e) {
 //			０除算は処理をせず強制的に０点。
 		}
